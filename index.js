4e6a071e5848155d3d409e35408946be8af0ab06
Dana Silver <dsilver1221@gmail.com>
2016-03-05 14:20:48 -0500

    Add support to pass units as second argument to tinyHumanTime.

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const units = {
 };
 
 function tinyHumanTime(t1, t2, u) {
-  u = u || 'long';
+  u = typeof arguments[arguments.length - 1] === 'string' ? arguments[arguments.length - 1] : 'long';
   t1 = Array.isArray(t1) ? t1[0] * 1e3 + t1[1] / 1e6 : t1;
   t2 = Array.isArray(t2) ? t2[0] * 1e3 + t2[1] / 1e6 : t2;
-  let milli = typeof t2 === 'undefined' ? t1 : Math.abs(t2 - t1);
+  let milli = isNaN(+t2) ? t1 : Math.abs(t2 - t1);
 
   if (milli < 0)             return units[u][0];
   if (milli === 0)           return units[u][1];
@@ -23,9 +23,11 @@ function tinyHumanTime(t1, t2, u) {
   if ((milli /= 7) < 52)     return format(Math.floor(milli), units[u][9], u);
   return format(Math.floor(milli / 52), units[u][10], u);
 }
-tinyHumanTime.short = function short (t1, t2) {
+
+tinyHumanTime.short = function short(t1, t2) {
   return tinyHumanTime(t1, t2, 'short');
 };
+
 module.exports = tinyHumanTime;
 
 function format(n, unit, mode) {
